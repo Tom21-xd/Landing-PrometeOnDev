8f6f6b034fe45a94a7738d2055f951f77137aa86
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-05-12 00:51:00 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,5 +12,7 @@ export default defineConfig({
     ssr: {
       noExternal: ['@fontsource-variable/onest']
     }
-  }
+  },
+
+  integrations: [react()]
 });
