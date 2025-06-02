fc45bc6f4b5846bae0842e0de5fcba407a48cfab
Johan Steven Ramirez Murcia <johan05182002.com@gmail.com>
2025-06-02 12:05:32 -0500

    fix (config/image)
    Se agregop una linea en el config con el fin de soluicionar el problema actual con las .env en vercel, se corrigio el nombre de las imagenes que estaban en mayus

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel"; //
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://prometeondev.com",
+  output: "server", 
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
